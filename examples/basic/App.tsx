266a34cad412d1febeec8fdfcf44ad54c9659ae1
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2019-02-19 19:44:40 +0100

    update readme, add example, fix config
    Signed-off-by: Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>

---
+++ examples/basic/App.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import {
+  createActions,
+  useModule,
+  createEpic,
+  createReducer,
+  useActions,
+  useMappedState,
+} from 'typeless';
+
+const MODULE = 'sample';
+
+const MyActions = createActions(MODULE, {
+  ping: null,
+  pong: (date: Date) => ({ payload: { date } }),
+});
+
+const epic = createEpic(MODULE).on(MyActions.ping, () =>
+  of(MyActions.pong(new Date())).pipe(delay(500))
+);
+
+declare module 'typeless/types' {
+  interface DefaultState {
+    sample: {
+      isPinging: boolean;
+      lastPongAt: Date;
+    };
+  }
+}
+
+const initialState = {
+  isPinging: false,
+  lastPongAt: null,
+};
+
+const reducer = createReducer(initialState)
+  .on(MyActions.ping, state => {
+    state.isPinging = true;
+  })
+  .on(MyActions.pong, (state, { date }) => {
+    state.isPinging = false;
+    state.lastPongAt = date;
+  });
+
+export function App() {
+  useModule({
+    epic,
+    reducer,
+    reducerPath: ['sample'],
+  });
+
+  const { ping } = useActions(MyActions);
+  const { isPinging, lastPongAt } = useMappedState(state => state.sample);
+
+  return (
+    <div>
+      {isPinging ? (
+        <button disabled>pinging...</button>
+      ) : (
+        <button onClick={ping}>ping</button>
+      )}
+      {lastPongAt && <div>last pong at {lastPongAt.toLocaleTimeString()}</div>}
+    </div>
+  );
+}
--- src/TypelessContext.ts
+++ src/TypelessContext.ts
@@ -1,9 +1,9 @@
-import React from 'react';
+import { createContext } from 'react';
 import { RootReducer } from './RootReducer';
 import { Store } from 'redux';
 import { RootEpic } from './RootEpic';
 
-export const TypelessContext = React.createContext({
+export const TypelessContext = createContext({
   rootEpic: null as RootEpic<any>,
   rootReducer: null as RootReducer<any>,
   store: null as Store<any>,
